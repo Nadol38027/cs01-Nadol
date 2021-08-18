6bff6612012aa1eef277500e5d84aee0c4a2962d
Nadol38027 <38027@virtual.prc.ac.th>
2021-08-18 12:08:04 +0700

    Create cs01-15.py

---
+++ cs01-15.py
@@ -0,0 +1,10 @@
+def my_list():
+    thislist = [5,10,15,20,25,50,20]
+
+    for i in range(len(thislist)):
+        print(i)
+        if (thislist[i] == 20):
+            thislist[i] = 200
+            print(thislist)
+
+print(my_list())
